a488dbdc2d97591d203270fa6f0daa226b62f9d6
Matthew Bowker <matthewrbowker@me.com>
2015-02-22 23:31:48 -0700

    CS 3300 - HW 2 complete

--- CS3300-Software Engineering 1/HW2/app/controllers/projects_controller.rb
+++ CS3300-Software Engineering 1/HW2/app/controllers/projects_controller.rb
@@ -7,7 +7,52 @@ class ProjectsController < ApplicationController
   end
 
   def index
-    @projects = Project.all
+    @all_users = ['Coulson','Ward','May','Skye','Fitz','Simmons'];
+
+    if params[:unset] != nil
+      session.clear();
+      redirect_to(projects_path)
+    end
+
+    if params[:sort] != nil
+      @sort = params[:sort]
+      session[:sort] = @sort;
+    elsif session[:sort] != nil
+      @sort = session[:sort];
+    else
+      @sort = "";
+    end
+
+    if params["users"].is_a? Hash
+      @params = params["users"];
+      session[:users] = @params
+    elsif session[:users].is_a? Hash
+      @params = session[:users]
+    else
+      @params = ""
+    end
+
+    if params["users"] != session[:users]||params[:sort] != session[:sort]
+      redirect_to(projects_path({:sort => @sort, :users => @params}))
+    end
+
+    if (@sort == "title")
+      order = "title"
+      @title = "hilite"
+    elsif (@sort == "date")
+      order = "due_date"
+      @date = "hilite"
+    else
+      order = "";
+    end
+
+    if @params.is_a? Hash
+      @projects = Project.all(:order => order, :conditions => { :user => @params.keys});
+      @checks = Hash[@params.keys.map {|x| [x, 1]}]
+    else
+      @projects = Project.all(:order => order);
+      @checks = Hash[@all_users.map {|x| [x, 1]}]
+    end
   end
 
   def new
--- CS3300-Software Engineering 1/HW2/config/routes.rb
+++ CS3300-Software Engineering 1/HW2/config/routes.rb
@@ -14,6 +14,7 @@ Rottenpotatoes::Application.routes.draw do
   #   resources :products
   resources :projects
   root :to => redirect('/projects')
+
   # Sample resource route with options:
   #   resources :products do
   #     member do
